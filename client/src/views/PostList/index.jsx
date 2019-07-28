ab97245d2f934ef18c3fa0da137de0bc0aba1359
Manish Sinha <arithmetic@gmail.com>
2019-07-28 15:08:43 -0700

    brand pull up

--- client/src/views/PostList/index.jsx
+++ client/src/views/PostList/index.jsx
@@ -3,13 +3,25 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import React from "react";
 import exitIntent from 'exit-intent'
-import { Modal, Button, Image, Header } from 'semantic-ui-react'
+import { Divider, Modal, Button, Image, Header } from 'semantic-ui-react'
 import { slide as Menu } from 'react-burger-menu'
+import { QueryOpts, ChildDataProps, withApollo, graphql, compose } from 'react-apollo'
 
+const categoryQuery = gql`
+  query search($brand: String!) {
+    search(brand: $brand) {
+      id
+      heroUrl
+      logoUrl
+      ctaText
+      url
+    }
+  }
+`
 
+const queryString = require('query-string');
 
 class ReportObstruction extends React.Component {
-
     constructor(props) {
         super(props)
 
@@ -30,15 +42,35 @@ class ReportObstruction extends React.Component {
     }
 
     render = () => {
+        const {data} = this.props
+
         return (
             <>
             <Menu right isOpen={this.state.visible} noOverlay customBurgerIcon={ false }>
-            <h2>Whoa whoa whoa</h2>
-            <p>blah</p>
-            </Menu>
+            {data.search && data.search.map((brand,i) => (
+                <>
+                <Image src={brand.heroUrl}/>
+                <br/>
+                <Image src={brand.logoUrl}/>
+                <br/>
+                <a target="_blank" href={brand.url}><Button>{brand.ctaText}</Button></a>
+                <Divider/>
+                </>
+                )
+            )}
+            
+                </Menu>
             </>
         )
     }
 }
 
-export default ReportObstruction
+const queryOptions = {
+    options: props => ({
+        variables: {
+            brand: queryString.parse(props.location.search).brand
+        },
+    }),
+}
+
+export default graphql(categoryQuery, queryOptions)(ReportObstruction)
--- server/kollab/graphql/schema.py
+++ server/kollab/graphql/schema.py
@@ -1,8 +1,24 @@
 import graphene
 
+from catalogue.models import BrandAssociations, Brand
+from graphene_django.types import DjangoObjectType
+
+
+class BrandType(DjangoObjectType):
+    class Meta:
+        model = Brand
+
 
 class Query(graphene.ObjectType):
-    yay = graphene.String()
+    search = graphene.List(BrandType, brand=graphene.String())
 
-    def resolve_yay(self, info, **kwargs):
-        return "woot"
+    def resolve_search(self, info, **kwargs):
+        try:
+            b = Brand.objects.get(name__iexact=kwargs.get('brand').lower())
+            return Brand.objects.filter(id__in=BrandAssociations.objects.filter(source=b).values_list('destination'))
+        except Brand.DoesNotExist:
+            return None
+        except BrandAssociations.DoesNotExist:
+            return None
+
+        return None
