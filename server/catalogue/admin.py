f9828a286907c6cd44415ffadcad143d0df4acc4
Manish Sinha <arithmetic@gmail.com>
2019-07-28 12:13:58 -0700

    brand models

--- server/catalogue/admin.py
+++ server/catalogue/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Brand, BrandAssociations
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'url', 'logo_url', 'single_liner', 'cta_text', 'date_created', )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_created', )
+
+
+class BrandAssociationAdmin(admin.ModelAdmin):
+    list_display = ('source', 'destination', 'date_created', )
+    ordering = ['source', 'destination']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(BrandAssociations, BrandAssociationAdmin)
--- server/catalogue/models.py
+++ server/catalogue/models.py
@@ -3,8 +3,38 @@ from django.db import models
 
 
 class Category(models.Model):
-    display_name = models.CharField(max_length=128)
-    slug = models.CharField(max_length=128)
-    image_url = models.CharField(max_length=256, blank=True, null=True)
+    name = models.CharField(max_length=256)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name', )
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    url = models.CharField(max_length=256)
+    logo_url = models.CharField(max_length=512)
+    single_liner = models.CharField(max_length=256)
+    cta_text = models.CharField(max_length=256)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+class BrandAssociations(models.Model):
+    source = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='source')
+    destination = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='destination')
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('source__name', 'destination__name', )
+
+    def __str__(self):
+        return f'{self.source.name} -> {self.destination.name}'
